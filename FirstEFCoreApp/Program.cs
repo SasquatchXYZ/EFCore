efb6ccbaa3cf9faa8e0faa6c657341b3f0aaa870
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-05-05 23:11:41 -0400

    Finish out the first app

--- FirstEFCoreApp/Program.cs
+++ FirstEFCoreApp/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+using FirstEFCoreApp;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(
+    "Commands: l (List), u (Change Url), r (Reset DB) and e (Exit) - add `-l` to the first two for logs");
+
+Console.Write(
+    "Checking if database exists... ");
+
+Console.WriteLine(Commands.WipeCreateSeed(true)
+    ? "Created database and seeded it"
+    : "Database exists.");
+
+do
+{
+    Console.Write("> ");
+    var command = Console.ReadLine();
+    switch (command)
+    {
+        case "l":
+            Commands.ListAll();
+            break;
+        case "u":
+            Commands.ChangeWebUrl();
+            break;
+        case "l -l":
+            Commands.ListAllWithLogs();
+            break;
+        case "u -l":
+            Commands.ChangeWebUrlWithLogs();
+            break;
+        case "r":
+            Commands.WipeCreateSeed(false);
+            break;
+        case "e":
+            return;
+        default:
+            Console.WriteLine("Unknown command.");
+            break;
+    }
+} while (true);
