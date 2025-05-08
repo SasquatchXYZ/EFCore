742c64099d74c51fdb84cf1822ae740d38afd464
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-05-07 23:09:08 -0400

    Add EfClasses for the tables

---
+++ DataLayer/EfClasses/BookAuthor.cs
@@ -0,0 +1,12 @@
+namespace DataLayer.EfClasses;
+
+public class BookAuthor
+{
+    public int BookId { get; set; }
+    public int AuthorId { get; set; }
+    public byte Order { get; set; }
+
+    // Relationships
+    public Book Book { get; set; }
+    public Author Author { get; set; }
+}
---
+++ DataLayer/EfClasses/Tag.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.EfClasses;
+
+public class Tag
+{
+    [Key] [Required] [MaxLength(40)] public string TagId { get; set; }
+
+    public ICollection<Book> Books { get; set; }
+}
