43336436e3af202d822d5bb5384de68e1c8f2c8e
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-05-07 23:31:38 -0400

    Add DbContext

---
+++ DataLayer/EfCode/EfCoreContext.cs
@@ -0,0 +1,37 @@
+using DataLayer.EfClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.EfCode;
+
+public class EfCoreContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<PriceOffer> PriceOffers { get; set; }
+
+    public EfCoreContext(DbContextOptions<EfCoreContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BookAuthor>()
+            .HasKey(bookAuthor => new
+            {
+                bookAuthor.BookId,
+                bookAuthor.AuthorId
+            });
+
+        // modelBuilder.Entity<LineItem>()
+        //     .HasOne(p => p.ChosenBook)
+        //     .WithMany()
+        //     .OnDelete(DeleteBehavior.Restrict);
+        //
+        // modelBuilder.Entity<Book>()
+        //     .HasQueryFilter(p => !p.SoftDeleted);
+        //
+        // modelBuilder.Entity<Order>()
+        //     .HasQueryFilter(x => x.CustomerId == _userId);
+    }
+}
