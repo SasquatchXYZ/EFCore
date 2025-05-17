99a6fc15c2bbac5938d2892a54be96e78a6fd399
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-05-16 23:53:58 -0400

    Change book publication date

---
+++ ServiceLayer/AdminServices/IChangePubDateService.cs
@@ -0,0 +1,9 @@
+using DataLayer.EfClasses;
+
+namespace ServiceLayer.AdminServices;
+
+public interface IChangePubDateService
+{
+    ChangePubDateDto GetOriginal(int id);
+    Book UpdateBook(ChangePubDateDto dto);
+}
