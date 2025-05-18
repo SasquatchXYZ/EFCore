d68ba520460d36f22ccc468cb7a8a1cd68de8665
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-05-17 22:51:18 -0400

    New DataLayer items for orders

--- DataLayer/EfCode/EfCoreContext.cs
+++ DataLayer/EfCode/EfCoreContext.cs
@@ -7,8 +7,9 @@ public class EfCoreContext : DbContext
 {
     public DbSet<Book> Books { get; set; }
     public DbSet<Author> Authors { get; set; }
-    public DbSet<Tag> Tags { get; set; }
     public DbSet<PriceOffer> PriceOffers { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
     public EfCoreContext(DbContextOptions<EfCoreContext> options) : base(options)
     {
@@ -32,6 +33,6 @@ public class EfCoreContext : DbContext
             .HasQueryFilter(p => !p.SoftDeleted);
 
         // modelBuilder.Entity<Order>()
-        //     .HasQueryFilter(x => x.CustomerId == _userId);
+        // .HasQueryFilter(x => x.CustomerId == _userId);
     }
 }
