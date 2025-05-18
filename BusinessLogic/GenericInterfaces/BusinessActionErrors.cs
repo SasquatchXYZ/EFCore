59415909588119a4af2d02377d0487706369dba0
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-05-17 22:16:40 -0400

    Add abstract class for validation handling

---
+++ BusinessLogic/GenericInterfaces/BusinessActionErrors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogic.GenericInterfaces;
+
+public abstract class BusinessActionErrors
+{
+    private readonly List<ValidationResult> _errors = [];
+
+    public IImmutableList<ValidationResult> Errors => _errors.ToImmutableList();
+
+    public bool HasErrors => _errors.Count != 0;
+
+    protected void AddError(string errorMessage, params string[] propertyNames) =>
+        _errors.Add(new ValidationResult(errorMessage, propertyNames));
+}
